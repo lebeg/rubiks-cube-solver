c4d9319aba03bf533faf3781b1d512f49e1adca4
Anton Chernov <anton.chernov@maplecroft.com>
2022-05-04 12:41:38 +0100

    Added intended format failure

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include "src/Cube.h"
 #include <iostream>
 
-int main()
-{
+int main()      {
     Cube cube;
     std::cout << cube << std::endl;
     std::cout << "Cube is " << (cube.solved() ? "solved" : "unsolved") << std::endl;
